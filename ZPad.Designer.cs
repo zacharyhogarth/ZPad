bcfd49c95a7a567864955385b39e4b11fe1d3ab5
zacharyhogarth <zachary.hogarth.comms@gmail.com>
2016-09-15 06:08:44 -0400

    Initial Upload

---
+++ ZPad.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ZPad
+{
+    partial class ZPad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ZPad));
+            this.rteTextEditor = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnSave = new System.Windows.Forms.ToolStripButton();
+            this.btnLoad = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rteTextEditor
+            // 
+            this.rteTextEditor.AcceptsTab = true;
+            this.rteTextEditor.AutoWordSelection = true;
+            this.rteTextEditor.EnableAutoDragDrop = true;
+            this.rteTextEditor.Location = new System.Drawing.Point(12, 37);
+            this.rteTextEditor.Name = "rteTextEditor";
+            this.rteTextEditor.Size = new System.Drawing.Size(742, 645);
+            this.rteTextEditor.TabIndex = 0;
+            this.rteTextEditor.Text = "";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnSave,
+            this.btnLoad});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(766, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnSave
+            // 
+            this.btnSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnSave.Image = ((System.Drawing.Image)(resources.GetObject("btnSave.Image")));
+            this.btnSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(23, 22);
+            this.btnSave.Text = "Save";
+            this.btnSave.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnLoad.Image = ((System.Drawing.Image)(resources.GetObject("btnLoad.Image")));
+            this.btnLoad.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(23, 22);
+            this.btnLoad.Text = "Load";
+            this.btnLoad.ToolTipText = "Load text file";
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // ZPad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(766, 685);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.rteTextEditor);
+            this.Name = "ZPad";
+            this.Text = "ZPad";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rteTextEditor;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnSave;
+        private System.Windows.Forms.ToolStripButton btnLoad;
+    }
+}
+
---
+++ ZPad.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZPad
+{
+    public partial class ZPad : Form
+    {
+        public ZPad()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDialog1 = new SaveFileDialog();
+            saveDialog1.Filter = "Text Document|*.txt|All Files|*.*";
+            saveDialog1.Title = "Save text file...";
+            saveDialog1.ShowDialog();
+
+            if (saveDialog1.FileName != "")
+            {
+                System.IO.File.WriteAllText(saveDialog1.FileName, rteTextEditor.Text);
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog1 = new OpenFileDialog();
+            openDialog1.Filter = "Text Document|*.txt|All Files|*.*";
+            openDialog1.Title = "Open text file...";
+            openDialog1.ShowDialog();
+
+            if (openDialog1.FileName != "")
+            {
+                rteTextEditor.Text = System.IO.File.ReadAllText(openDialog1.FileName);
+            }
+        }
+    }
+}
